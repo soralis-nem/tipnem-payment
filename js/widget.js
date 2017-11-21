e201860083242af15ea35b5d2b2ee70205838549
unknown <m100103m@gmail.com>
2017-11-21 21:31:28 +0900

    test widget

--- js/widget.js
+++ js/widget.js
@@ -1,67 +1,90 @@
-function GetQueryString() {
-	var result= {}
-	;
-	if (1 < window.location.search.length) {
-		var query=window.location.search.substring(1);
-		var parameters=query.split('&');
-		for (var i=0;
-		i < parameters.length;
-		i++) {
-			var element=parameters[i].split('=');
-			var paramName=decodeURIComponent(element[0]);
-			var paramValue=decodeURIComponent(element[1]);
-			if (typeof paramValue==="undefined") {
-				result[paramName]="";
-			}
-			else {
-				result[paramName]=paramValue;
-			}
+function GetScriptParams()
+{
+    var scripts = document.getElementsByTagName( 'script' );
+    var src = scripts[ scripts.length - 1 ].src;
+
+    var query = src.substring( src.indexOf( '?' ) + 1 );
+    var parameters = query.split( '&' );
+
+    var result = new Object();
+    for( var i = 0; i < parameters.length; i++ )
+    {
+        var element = parameters[ i ].split( '=' );
+
+        var paramName = decodeURIComponent( element[ 0 ] );
+        var paramValue = decodeURIComponent( element[ 1 ] );
+
+		if (typeof paramValue==="undefined") {
+			result[paramName]="";
 		}
-	}
-	return result;
+		else {
+			result[paramName]=paramValue;
+		}
+    }
+
+    return result;
 }
 
 
+function createqr(to,amount,name,comment){
+	var to = encodeURIComponent(tipnem_popup.to.value);
+	var amount = tipnem_popup.amount.value;
+	var name = encodeURIComponent(tipnem_popup.name.value);
+	var comment = encodeURIComponent(tipnem_popup.comment.value);
+	var url = "https://twitter.com/intent/tweet?text=%40tipnem%20tip%20"+to+"%20"+amount+"%20"+name+"%20"+comment+"&hashtags=tipnem%e6%b1%ba%e6%b8%88";
+	var qr = "http://chart.apis.google.com/chart?chs=547x547&cht=qr&chl="+encodeURIComponent(url);
+	return qr;
+}
+function createurl(to,amount,name,comment){
+	var to = encodeURIComponent(tipnem_popup.to.value);
+	var amount = tipnem_popup.amount.value;
+	var name = encodeURIComponent(tipnem_popup.name.value);
+	var comment = encodeURIComponent(tipnem_popup.comment.value);
+	var url = "https://twitter.com/intent/tweet?text=%40tipnem%20tip%20"+to+"%20"+amount+"%20"+name+"%20"+comment+"&hashtags=tipnem%e6%b1%ba%e6%b8%88";
+	return url;
+}
 
-$(function() {
-	$('#show').click(function(e) {
-		$('#popup, #layer').show();
-		var args=GetQueryString();
-		form1.to.value=args["to"];
-		form1.amount.value=args["amount"];
-		form1.name.value=args["name"];
-		form1.comment.value=args["comment"];
-	}
-	);
-
-	$('#close').click(function(e) {
-		$('#popup, #layer').hide();
-	}
-	);
-
-	$('#show-qr').click(function(e) {
-		var to=encodeURIComponent(form1.to.value);
-		var amount=form1.amount.value;
-		var name=encodeURIComponent(form1.name.value);
-		var comment=encodeURIComponent(form1.comment.value);
-		$('#qrcode').show();
-		qrcode.src =createqr(to, amount, name, comment);
-	}
-	);
-
-	$('#tip').click(function(e) {
-		$('#popup, #layer').hide();
-		var to=encodeURIComponent(form1.to.value);
-		var amount=form1.amount.value;
-		var name=encodeURIComponent(form1.name.value);
-		var comment=encodeURIComponent(form1.comment.value);
-		var url = createurl(to, amount, name, comment);
-		window.open(url);
-	}
-	);
+function tipnem_show()
+{
+	document.getElementById("tipnem_back").style.display="block";
+	document.getElementById("tipnem_widget").style.display="block";
 
 }
-);
+
+function tipnem_close()
+{
+	document.getElementById("tipnem_back").style.display="none";
+	document.getElementById("tipnem_widget").style.display="none";
+}
+function tipnem_showqr()
+{
+	var to=encodeURIComponent(tipnem_popup.to.value);
+	var amount=tipnem_popup.amount.value;
+	var name=encodeURIComponent(tipnem_popup.name.value);
+	var comment=encodeURIComponent(tipnem_popup.comment.value);
+	document.getElementById("tipnem_qr").style.display="block";
+	tipnem_qr.src =createqr(to, amount, name, comment);
+}
+function tipnem_tip()
+{
+	tipnem_close();
+	var to=encodeURIComponent(tipnem_popup.to.value);
+	var amount=tipnem_popup.amount.value;
+	var name=encodeURIComponent(tipnem_popup.name.value);
+	var comment=encodeURIComponent(tipnem_popup.comment.value);
+	var url = createurl(to, amount, name, comment);
+	window.open(url);
+}
+
+
 
 window.onload=function () {
+
+
+	$('#tipnem-widget').html("<button onclick='tipnem_show ();' >TipNemで寄付する</button><div id='tipnem_back' style='display: none;'></div><div id='tipnem_widget' style='display: none;'><img src='img/close.png'  id='tipnem_close_button' onclick='tipnem_close();'><form name='tipnem_popup'><div class='mui-textfield' name='input' ><input type='text' name='to' required><label>送る相手</label></div><div class='mui-textfield' name='input' ><input type='text' name='amount' required><label>数量</label></div><div class='mui-textfield' name='input' ><input type='text' name='name'  required><label>mosaic名</label></div><div class='mui-textfield' name='input' ><input type='text' name='comment' ><label>コメント</label></div><button type='button' onclick='tipnem_tip ();' class='mui-btn mui-btn--primary' >送る</button><button type='button' onclick='tipnem_showqr ();' class='mui-btn mui-btn--primary' >QR</button></form><img src='' width='256' height='256' id='tipnem_qr' style='display: none;' ></div>");
+	var args=GetScriptParams();
+	tipnem_popup.to.value=args["to"];
+	tipnem_popup.amount.value=args["amount"];
+	tipnem_popup.name.value=args["name"];
+	tipnem_popup.comment.value=args["comment"];
 }
